6bec3566b272821e5f2f8b7ddd1049735d5a484f
LayneTheEpic <50891500+LayneTheEpic@users.noreply.github.com>
2023-08-27 16:57:44 -0400

    instead useAssignLastCommitToElement

--- src/generateLastCommitText.ts
+++ src/generateLastCommitText.ts
@@ -11,18 +11,14 @@ type GithubCommit = {
 
 async function fetchLastCommit(repository: string): Promise<GithubCommit | null> {
 	return await fetch(`https://api.github.com/repos/${repository}/commits?per_page=1`)
-		.then(res => res.ok ? res.json() : null)
-		// if the response is not ok, then this will error trying to perform null[0]
-		.then((res: GithubCommit[]) => res[0])
-		// ...and be caught here
-		.catch(() => null);
+		.then(res => res.ok ? res.json() : null) // if res is not ok, return null;
+		.then((res: GithubCommit[]) => res[0]) // then this will error trying to perform null[0]
+		.catch(() => null); // and be caught here
 }
 
 
 
-// possible philosophy improvement: export a "assignElementToLastCommit" method accepting an element and a repository
-// i like this pattern more than directly setting textContent tho
-export default async function generateLastCommitText(repository: string) {
+async function generateLastCommitText(repository: string) {
 	const lastCommit = await fetchLastCommit(repository);
 
 	if(lastCommit === null) {
@@ -36,4 +32,10 @@ export default async function generateLastCommitText(repository: string) {
 	const formattedHash = lastCommit.sha.slice(0, 7);
 
 	return `Last updated ${formattedDate} (commit ${formattedHash})`;
+}
+
+
+
+export default async function assignLastCommitToElement(element: HTMLElement, repository: string) {
+	element.textContent = await generateLastCommitText(repository);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import generateLastCommitText from "./generateLastCommitText.js";
+import assignLastCommitToElement from "./generateLastCommitText.js";
 
 
 
 const lastUpdatedSpan = document.getElementById("last-updated")!;
 
-
-
-lastUpdatedSpan.textContent = await generateLastCommitText("jessamine-dto/jessamine-dto.github.io");
+assignLastCommitToElement(lastUpdatedSpan, "jessamine-dto/jessamine-dto.github.io");
